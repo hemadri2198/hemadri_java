090c15484d426205c2b7977585b46c5f2adf39da
hemadri reddy <35724308+hemadri2198@users.noreply.github.com>
2018-09-12 13:07:25 +0530

    Add files via upload

---
+++ hemadri1.java
@@ -0,0 +1,43 @@
+public class hemadri1 {
+
+   public static void main(String args[]){
+      int num=70;
+      if( num < 100 ){
+	  
+	  System.out.println("number is less than 100");
+      }
+  
+
+      int number = 10;
+	 
+      if (number > 0) {
+         System.out.println("Number is positive.");
+      }
+      else {
+         System.out.println("Number is not positive.");
+      }
+ 
+      System.out.println("This statement is always executed.");
+  
+
+         int i=10;
+         while(i>1){
+              System.out.println(i);
+              i--;
+         }
+    
+
+      int i1= 1;
+	   
+      while (i<= 10) {
+         System.out.println("Line " + i);
+         ++i;
+      }
+   
+
+      int j;
+      for (int j=1;j<=10;++j) {
+         System.out.println("Line " + j);
+      }
+   }
+}
